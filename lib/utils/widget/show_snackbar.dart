0061acf5fd5509ceb9baf21ad224a285824068bd
Ismail <ismail@silkron.com>
2024-09-23 09:56:10 +0800

    added snackbar bg color

--- lib/utils/widget/show_snackbar.dart
+++ lib/utils/widget/show_snackbar.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 void showSnackbar({
   required BuildContext context,
   required String message,
+  Color bgColor = Colors.green,
 }) {
   ScaffoldMessenger.of(context)
     ..hideCurrentSnackBar()
-    ..showSnackBar(SnackBar(content: Text(message)));
+    ..showSnackBar(SnackBar(
+      content: Text(message),
+      backgroundColor: bgColor,
+    ));
 }
