a41842b8d1c1dd0672f3f0d8cba191db2722f911
Ismail <ismail@silkron.com>
2024-09-23 09:37:07 +0800

    added bloc log configuratin

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ojt_irfan/config/router/app_router.dart';
 
 import 'package:ojt_irfan/config/theme/app_theme.dart';
 import 'package:ojt_irfan/screen/home/view/home_screen.dart';
 import 'package:ojt_irfan/service/database/database_config.dart';
+import 'package:ojt_irfan/utils/simple_bloc_delegate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseConfig().initDatabase();
+  Bloc.observer = SimpleBlocDelegate();
   runApp(const MyApp());
 }
 
